576368d4db6289e359c93934b0c2310689d043e0
Carmo M. Romero <carmoromero52@gmail.com>
2022-08-15 17:26:06 -0300

    cleaning create-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='amarelo'>
-          Olá mundo React 3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
